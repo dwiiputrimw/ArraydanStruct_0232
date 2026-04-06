1ad49c92203329e5755a1a0471bd196f854f8489
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-06 20:12:47 +0700

    menampilkan data lengkap mahasiswa

--- structgetLine.cpp
+++ structgetLine.cpp
@@ -27,6 +27,13 @@ int main() {
     cin >> mhs.alamat.kota;
     cout << "Umur : " ; 
     cin >> mhs.umur;
-    cout << endl; 
+    cout << endl;
+    // tampilkan 
+    cout << "Menampilkan Data" << endl; 
+    cout << "Nama : " << mhs.nama <<endl; 
+    cout << "Desa : " << mhs.alamat.desa <<endl; 
+    cout << "Kota : " << mhs.alamat.kota <<endl; 
+    cout << "Umur : " << mhs.umur << endl;
+
 
 }
