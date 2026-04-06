a4181bfefa0efaf68af31663ccc975175ad46c99
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-06 20:02:57 +0700

    setup program dan deklarasi struct detailAlamat dan orang

--- structNEW2.cpp
+++ structNEW2.cpp
@@ -16,22 +16,4 @@ struct Orang
 
 int main() {
     Orang mhs; 
-    // Mengisi data
-    cout << "Mengisi Data" << endl; 
-    cout << "Nama : " ; 
-    cin >> mhs.nama;
-    cout << "Desa : " ; 
-    cin >> mhs.alamat.desa;
-    cout << "Kota : " ; 
-    cin >> mhs.alamat.kota;
-    cout << "Umur : " ; 
-    cin >> mhs.umur;
-    cout << endl; 
-    // tampilkan 
-    cout << "Menampilkan Data" << endl; 
-    cout << "Nama : " << mhs.nama <<endl; 
-    cout << "Desa : " << mhs.alamat.desa <<endl; 
-    cout << "Kota : " << mhs.alamat.kota <<endl; 
-    cout << "Umur : " << mhs.umur << endl;
-
 }
