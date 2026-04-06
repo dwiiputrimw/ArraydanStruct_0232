f7f38fd8833bab95a68e13db0caaa95c172d6e0a
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-04-06 19:52:14 +0700

    menambahkan perulangan untuk input dan output array

--- arrayNEW.cpp
+++ arrayNEW.cpp
@@ -32,4 +32,17 @@ int main() {
     cout << "Data Keempat = " << kamu[3] << endl; 
     cout << "Data Kelima = " << kamu[4] << endl; 
     cout << endl; 
-}
+
+     // Mengisi dengan menggunakan perulangan for 
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data index ke-" << i << " = ";
+        cin >> kamu[i];
+    }
+    cout << endl; 
+    // Menampilkan dengan menggunakan perulangan for  
+    for (int i = 0; i <= 4; i++)
+    {
+        cout << "Data ke-" << i+1 << " = " << kamu[i] << endl;
+    }
+} 
